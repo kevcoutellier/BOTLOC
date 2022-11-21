cc5126a1127483938ee09090015ce01c923cce2b
Kevin Coutellier <kev.coutellier@gmail.com>
2022-11-21 16:42:17 +0100

    addcolumn

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -1,3 +1,9 @@
 class Bot < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true, uniqueness: true
+  validates :rating, inclusion: { in: [1..5], allow_nil: false }
+  validates :descritpion, presence: true
+  validates :price, prensence: true
+
+
 end
