3d3f101488f7aa1b27f34a8173b233adc66fb833
Kevin Coutellier <kev.coutellier@gmail.com>
2022-11-22 11:21:50 +0100

    test

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,18 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
   end
 
   def create
+    @bot = Bot.find(params [:bot_id])
+    @booking = Booking.new(booking_params)
+    @booking.bot = @bot
+    @booking.user = current_user
+    if
+      @booking.save
+      redirect_to booking_path(@booking)
+    else
+      redirect_to bot_path(@bot)
+    end
   end
 end
--- app/models/bot.rb
+++ app/models/bot.rb
@@ -1,9 +1,7 @@
 class Bot < ApplicationRecord
   belongs_to :user
   validates :name, presence: true, uniqueness: true
-  validates :rating, inclusion: { in: [1..5], allow_nil: false }
+  validates :rating, inclusion: { in: [1..5] }, allow_nil: false
   validates :descritpion, presence: true
   validates :price, prensence: true
-
-
 end
