bd8d565d8e1098b6a4e287f2eedd5bdf722e6aed
Kevin Coutellier <kev.coutellier@gmail.com>
2022-11-23 14:54:07 +0100

    Navbar a check, debut index booking

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   resources :bookings, only: [:new, :create]
   end
   resources :bookings, only: [:show]
+  resources :bookings, only: [:index]
   # # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # # Defines the root path route ("/")
