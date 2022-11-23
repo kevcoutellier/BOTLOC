e8d0c708b7b70ad5b1ca82f409271d65564071d1
Kevin Coutellier <kev.coutellier@gmail.com>
2022-11-23 12:38:42 +0100

    navbar done

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :bots, only: [:index, :new, :create, :show] do
-    resources :bookings, only: [:new, :create, :show]
+  resources :bookings, only: [:new, :create]
   end
+  resources :bookings, only: [:show]
   # # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # # Defines the root path route ("/")
