699b350b87daa6ff9747fc3559f9defa2123f07f
Teddy Muller <muller.teddy@icloud.com>
2022-11-24 16:27:32 +0100

    creation edit delete et mise en fonction create

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -2,7 +2,7 @@ class Bot < ApplicationRecord
   has_one_attached :photo
   belongs_to :user
   validates :name, presence: true, uniqueness: true
-  validates :rating, inclusion: { in: (1..5) }, allow_nil: false
+  # validates :rating, inclusion: { in: (1..5) }, allow_nil: false
   validates :description, presence: true
   validates :price, presence: true
   geocoded_by :address
