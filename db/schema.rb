613531a2de49bc8c6ed381ea71e9a15fea832b22
diana spieser <dianaspieser@gmail.com>
2022-11-25 13:09:32 +0100

    calendreir

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_23_153029) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_25_112539) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,8 +43,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_23_153029) do
   end
 
   create_table "bookings", force: :cascade do |t|
-    t.date "start"
-    t.date "end"
+    t.string "start"
+    t.string "end"
     t.bigint "user_id", null: false
     t.bigint "bot_id", null: false
     t.datetime "created_at", null: false
