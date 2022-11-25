a51dea1feec25c5a0dd689f8cc9da4ce9fc90449
diana spieser <dianaspieser@gmail.com>
2022-11-25 14:36:21 +0100

    laqt modifs

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -15,20 +15,3 @@ export default class extends Controller {
     });
   }
 }
-
-// import { Controller } from "@hotwired/stimulus"
-// import flatpickr from "flatpickr";
-// // Import the rangePlugin from the flatpickr library
-// import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
-
-// // Connects to data-controller="flatpickr"
-// export default class extends Controller {
-//   static targets = [ "startTime", "endTime" ]
-//   connect() {
-//     flatpickr(this.startTimeTarget, {
-//               enableTime: true,
-//               // Provide an id for the plugin to work
-//               plugins: [new rangePlugin({ input: "#end_time"})]})
-//     flatpickr(this.endTimeTarget, {})
-//   }
-// }
